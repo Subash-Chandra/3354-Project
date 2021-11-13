5daaeb329bcf301752efdda76f6e51c281e39766
Subash Chandra <meeetsubash@gmail.com>
2021-11-12 22:33:29 -0600

    style.css correct folder

--- index.php
+++ index.php
@@ -6,13 +6,15 @@ include("auth.php");
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Welcome Home</title>
-        <link rel="stylesheet" href="css/app.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Is This Even Real?</title>
+        <link rel="stylesheet" href="app.css" />
     </head>
     <body>
     <div class="container">
       <div id="home" class="flex-center flex-column">
-        <h1>Quick Quiz</h1>
+        <h1>Is This Even Real?</h1>
         <a href="/play.html" class="btn">Play </a>
         <a href="/leaderboard.html" class="btn">Leaderboard</a>
         <a href="/options.html" class="btn">Options</a>
