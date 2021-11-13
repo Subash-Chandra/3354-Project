e661e6d718fa7c20f3e50e97f9de16f751de688e
GrantBabin <grantbabin@gmail.com>
2021-11-12 19:56:41 -0600

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,7 +9,7 @@ if(isset($_POST["submit"])){
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
 
-    if(emptyInputSignup() !== false){
+    if(emptyInputSignup()){
         header("location: ../signup.html");
         exit();
     }
