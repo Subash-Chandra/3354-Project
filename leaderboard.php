45b6bd39cf23fd1a51eae2bf2ac09a6c9871b788
fleetingflatcher <yeffdawg@gmail.com>
2021-12-03 01:43:59 -0600

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -19,6 +19,7 @@ include("auth.php");
     
 </head>
 <body>
+    <h1>Leaderboard</h1>
     <?php
         require("db.php");
         $query = "SELECT `username` FROM `users` ORDER BY `highscore` DESC LIMIT 5;";
@@ -26,11 +27,11 @@ include("auth.php");
         if ($result == FALSE) die ("could not execute statement $query<br />");
 
         while($row = mysqli_fetch_row($result)) {
-            echo("got row");
+            echo($row[0]);
         }
         
     ?>
-    <h1>Leaderboard</h1>
+    
     
 </body>
 </html>
