35abc72c59b144a66ec18ffe8acd569c2954d402
GrantBabin <grantbabin@gmail.com>
2021-12-03 04:31:17 -0600

    Update play.js
    fixed quote shenanigans

--- play.js
+++ play.js
@@ -58,7 +58,7 @@ function showScores() {
     var gameOverHTML = "<h1>Result</h1>";
     gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
     gameOverHTML += '<div id="game" class="flex-center flex-column">'
-    gameOverHTML += '<div><button onclick="location.href="index.php"">Home</button></div></div';
+    gameOverHTML += '<div><button onclick="location.href=\'index.php\'"">Home</button></div></div';
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
