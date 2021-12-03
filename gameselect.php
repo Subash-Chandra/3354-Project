b1803a64ad01612c6835d876c70ba80e8d38f2c9
Subash Chandra <meeetsubash@gmail.com>
2021-12-03 01:29:33 -0600

    Update gameselect.php

--- gameselect.php
+++ gameselect.php
@@ -31,7 +31,7 @@ include("auth.php");
             </div>
             <script type="text/javascript" src="gameinfo.js"></script>
             <button onclick="submitAnswer(gametype.choice.value, gamemode.choice.value)">Submit</button>
-            <div></div>
+            <h2 id="padding">-</h2>
             <div><button onclick="location.href='index.php'">Home</button></div>
         </div>
     </div>
