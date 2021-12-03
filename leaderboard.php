2917b06316f094d62def6cb56325504a7d68cbdd
fleetingflatcher <yeffdawg@gmail.com>
2021-12-03 01:12:19 -0600

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -18,10 +18,11 @@ include("auth.php");
         require("db.php");
 
         $result = mysqli_query($con, "SELECT * FROM `users` 
-                        ORDER BY `highscore` DESC LIMIT 5;") 
-                        or die(mysql_error());
-        //echo $result;
+                        ORDER BY `highscore` DESC LIMIT 5;");
+                        //or die(mysql_error());
+        
         
     ?>
+    <h1>Leaderboard</h1>
 </body>
 </html>
