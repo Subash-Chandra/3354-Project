71d719acf7bc73bcc906586197104305bdda7abb
fleetingflatcher <yeffdawg@gmail.com>
2021-12-03 20:56:20 -0600

    cleanup

--- play.php
+++ play.php
@@ -7,11 +7,6 @@
 <link rel="stylesheet" href="play.css" />
 
 <?php
-error_reporting(-1);
-// Same as error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 'On');  //On or Off
-
 include ('auth.php');
 ?>
 
