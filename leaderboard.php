5ba63338e29b27484a01b4f0c600d9fca4bb7922
Subash Chandra <meeetsubash@gmail.com>
2021-12-03 19:10:13 -0600

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -40,7 +40,7 @@ include("auth.php");
     
     </div>
     <div class="tiny-container">
-    <h5 id="padding">-</h5>
+    <h5 id="padding"></h5>
     <div><button onclick="location.href='index.php'">Home</button></div>
     </div>
 </body>
