9a3a8e54ed8a25cc72a371d5b5b5835dd9ff8c91
fleetingflatcher <yeffdawg@gmail.com>
2021-12-03 23:26:02 -0600

    Update saveScore.php

--- saveScore.php
+++ saveScore.php
@@ -1,15 +1,17 @@
 <?php
 include('auth.php');
 require('db.php');
-
+echo ("Page start\n");
 if (isset($_POST["score"])) {
     $score = $_POST["score"];
     echo ("Found score of " + $score);
 }
 else $score = 3;
+echo("Score check done\n");
 if (isset($_SESSION["username"])) {
     echo ("Session username is set to " + $_SESSION["username"]);
 }
+echo("User check done\n");
 $currentUser = $_SESSION["username"];
 $query = "UPDATE `users` SET `highscore` = $score WHERE `username` = '$currentUser'";
 //echo ($query);
