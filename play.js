bb65bf7aa68ad32c0e224b987bf7addb8046c017
Subash Chandra <meeetsubash@gmail.com>
2021-12-05 04:59:33 -0600

    Update play.js

--- play.js
+++ play.js
@@ -107,7 +107,7 @@ $(document).ready(function() {
         gameOverHTML += '<img src="/images/fake/faces/' + images.fake + '" style="width:350px">';
         gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
         gameOverHTML += '<h3 style="margin-bottom:50px"> Thank You For Playing! </h3>';
-        gameOverHTML += '<div><button class="btn" onclick="location.href=\'play.php\'"">Try Again</button></div></div>';
+        gameOverHTML += '<div><button class="btn" onclick="location.href=\'play.php\'"">Try Again</button></div>';
         gameOverHTML += '<div><button class="btn" onclick="location.href=\'index.php\'"">Home</button></div></div>';
         var element = document.getElementById("quiz");
         element.innerHTML = gameOverHTML;
