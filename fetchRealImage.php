cbff7fa9dc5fafc5abe8cb3c421730357ccc4c49
fleetingflatcher <yeffdawg@gmail.com>
2021-12-05 03:37:14 -0600

    Proliferation of fetchImage php files; relocation of element reference in loop

--- fetchRealImage.php
+++ fetchRealImage.php
@@ -1,27 +1,17 @@
 <?php
-
 require("imageDB.php");
-$fake = $_POST['fake'];
-$numRowQuery;
-if ($fake) {
-    $numRowQuery = "SELECT * FROM `fake`";
-}
-else { $numRowQuery = "SELECT * FROM `real`"; }
+
+$numRowQuery = "SELECT * FROM `real`";
 
 $imageList = mysqli_query($imageDBCon, $numRowQuery);
 if ($imageList == FALSE) die ("could not execute statement $numRowQuery<br />");
 $i = random_int(0, mysqli_num_rows($imageList));
 
-$imageFetchQuery;
-if ($fake) {
-    $imageFetchQuery = "SELECT `filename` FROM `fake` WHERE `id`=$i";
-}
-else { $imageFetchQuery = "SELECT `filename` FROM `real` WHERE `id`=$i"; }
+$imageFetchQuery = "SELECT `filename` FROM `real` WHERE `id`=$i";
 
 $image = mysqli_query($imageDBCon, $imageFetchQuery);
 if ($image == FALSE) die ("could not execute statement $imageFetchQuery<br />");
 $response = mysqli_fetch_row($image)[0];
-
 
 echo json_encode($response);
 ?>
