f3509672f7dd15cd729ed71ea2d004ed48da9bb5
fleetingflatcher <yeffdawg@gmail.com>
2021-12-06 00:20:44 -0600

    Update saveScore.php

--- saveScore.php
+++ saveScore.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(-1);
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');  //On or Off
+
 include('auth.php');
 require('db.php');
 
