dd25bbed3706ada39f37b8bf01fbc468cecb5652
fleetingflatcher <yeffdawg@gmail.com>
2021-12-06 00:20:01 -0600

    Update saveScore.php

--- saveScore.php
+++ saveScore.php
@@ -6,13 +6,16 @@ require('db.php');
 if (isset($_POST["score"])) {
     $score = $_POST["score"];
 }
+else $score = 2;
 if (isset($_SESSION["username"])) {
     $currentUser = $_SESSION["username"];
 }
 $checkQuery = "SELECT `highscore` from `users` WHERE `username`='$currentUser'";
 $result = mysqli_query($con, $checkQuery);
-$currentHighscore = $result;
+echo ($result);
+$currentHighscore = mysqli_fetch_row($result);
 if ($score > $currentHighscore) {
+    echo ("score > current");
     $updateQuery = "UPDATE `users` SET `highscore` = $score WHERE `username` = '$currentUser'";
     mysqli_query($con, $updateQuery);
 }
